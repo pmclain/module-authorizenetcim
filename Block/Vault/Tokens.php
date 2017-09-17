fdc6e96b147c8eaa0f89215d1c39848abd60b64f
Patrick McLain <pmclain@somethingdigital.com>
2017-09-17 12:57:59 -0400

    Add MultiShipping Support for Vault Payment Option

---
+++ Block/Vault/Tokens.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Pmclain_AuthorizenetCim extension
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the GPL v3 License
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://www.gnu.org/licenses/gpl.txt
+ *
+ * @category  Pmclain
+ * @package   Pmclain_AuthorizenetCim
+ * @copyright Copyright (c) 2017
+ * @license   https://www.gnu.org/licenses/gpl.txt GPL v3 License
+ */
+
+namespace Pmclain\AuthorizenetCim\Block\Vault;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Vault\Model\CustomerTokenManagement;
+use Pmclain\AuthorizenetCim\Model\Ui\ConfigProvider;
+use Magento\Payment\Model\CcConfigProvider;
+
+class Tokens extends \Magento\Framework\View\Element\Template
+{
+    /** @var CustomerTokenManagement */
+    protected $customerTokenManagement;
+
+    /** @var CcConfigProvider */
+    protected $configProvider;
+
+    /** @var array */
+    protected $icons;
+
+    public function __construct(
+        Template\Context $context,
+        CustomerTokenManagement $customerTokenManagement,
+        CcConfigProvider $configProvider,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->customerTokenManagement = $customerTokenManagement;
+        $this->configProvider = $configProvider;
+    }
+
+    /** @return \Magento\Vault\Api\Data\PaymentTokenInterface[] */
+    public function getCustomerTokens()
+    {
+        $tokens = $this->customerTokenManagement->getCustomerSessionTokens();
+        $methodTokens = [];
+
+        foreach ($tokens as $token) {
+            if ($token->getPaymentMethodCode() === ConfigProvider::CODE) {
+                $methodTokens[] = $token;
+            }
+        }
+
+        return $methodTokens;
+    }
+
+    /**
+     * @param string $code
+     * @return false|array
+     */
+    public function getIcon($code)
+    {
+        if (isset($this->getIcons()[$code])) {
+            return $this->getIcons()[$code];
+        }
+
+        return false;
+    }
+
+    /** @return array */
+    protected function getIcons()
+    {
+        if ($this->icons) {
+            return $this->icons;
+        }
+
+        $this->icons = $this->configProvider->getIcons();
+
+        return $this->icons;
+    }
+}
